3484ad0c6eaa0f56a9e4f71ed7f887d5f1f6fe0f
pawzwo <pawelzwolinski@poczta.onet.pl>
2025-02-23 14:47:04 +0100

    finishGame implementation

---
+++ src/main/java/service/ScoreBoardService.java
@@ -0,0 +1,17 @@
+package service;
+
+import domain.Game;
+import domain.ScoreBoard;
+
+public class ScoreBoardService {
+
+    public void startGame(ScoreBoard scoreBoard, Game game) {
+        String key = game.getHomeTeam().getName() + game.getAwayTeam().getName();
+        scoreBoard.getGames().put(key, game);
+    }
+
+    public void finishGame(ScoreBoard scoreBoard, Game game) {
+        String key = game.getHomeTeam().getName() + game.getAwayTeam().getName();
+        scoreBoard.getGames().remove(key);
+    }
+}
