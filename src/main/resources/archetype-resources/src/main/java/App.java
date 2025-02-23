1f42420e46c54e4802a339c2b351328a7a29d42e
pawzwo <pawelzwolinski@poczta.onet.pl>
2025-02-23 11:41:16 +0100

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.pawelZ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
